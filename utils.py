5ccb6e3cbfda4ab8ff1824bb1922b8091695feaf
Bruno Lima <bgcl@ic.ufal.br>
2018-06-20 18:32:06 -0300

    Axis plot is correct. TODO: Change vectors from python array to numpy column vector

--- utils.py
+++ utils.py
@@ -6,8 +6,10 @@ Created on Wed Jun 20 14:30:20 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-### Performs multiplication from quaternions q1 and q2, returning the quaternion result q.
+""" Performs multiplication from quaternions q1 and q2, returning the quaternion result q. """
 def quatProd(q1, q2):
     # All quaternions q, q1 and q2 are represented as 1x4 row vectors
     q = np.zeros(4)
@@ -21,12 +23,51 @@ def quatProd(q1, q2):
     q[1:] = u0*v + v0*u + np.cross(u, v)  
     return q
 
-if __name__ == '__main__':
-    q1 = np.random.random(4)
-    q1 = q1 / np.linalg.norm(q1)
+""" Plot a line between two points given an axis with a specific color"""
+def plotLine(ax, p2, p1=[0, 0, 0], color='black', label=''):
+     ax.plot([p1[0], p2[0]],
+             [p1[1], p2[1]],
+             [p1[2], p2[2]], 
+             color=color)
+     if label:
+        ax.text(p2[0], p2[1], p2[2] , label)
+        
+
+""" Init and setup axis. Needed before plotting vectors. """    
+def initPlot():
+    # Define figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
     
-    q2 = np.random.random(4)
-    q2 = q2 / np.linalg.norm(q2)
+    # Defining origin and unit vectors
+    origin = [0, 0, 0]
+    x_axis = [1, 0, 0]  
+    y_axis = [0, 1, 0]
+    z_axis = [0, 0, 1]
+     
+    # Defining labels and initial view angle
+    ax.view_init(30, 90)
+
+    # Hide axes ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+
+    # Plot axis lines
+    plotLine(ax, x_axis, origin, 'red', 'X Axis')
+    plotLine(ax, y_axis, origin, 'green', 'Y Axis')
+    plotLine(ax, z_axis, origin, 'blue', 'Z Axis') 
+    plt.show()  
     
-    q = quatProd(q1, q2)
-    np.linalg.norm(q)
+    return ax
+    
+if __name__ == '__main__': 
+    # Generate a random normalized axis vector
+    w = np.random.rand(3, 1)
+    w_norm = np.linalg.norm(w)
+
+    # Plot lines
+    ax = initPlot()
+#    plotLine(ax, w_norm)
+    
+
