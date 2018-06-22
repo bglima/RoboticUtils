6cf1da04deace34ddf5179c7f5dad935982f0fb8
Bruno Lima <bg.lima@hotmail.com>
2018-06-21 23:08:09 -0300

    Rotation with axis angle working

--- exampleVectorRotation.py
+++ exampleVectorRotation.py
@@ -3,10 +3,26 @@ from utils import *
 if __name__ == '__main__':
     # Generate a random normalized axis vector
     w = np.random.rand(3, 1)
-    w_norm = w / np.linalg.norm(w)
+    w = w / np.linalg.norm(w)
 
-    # Plot lines
+    # Generate a fixed angle (easier to debug)
+    theta = math.pi / 2
+
+    # Generate a vector that will be rotated
+    v = np.random.rand(3, 1)
+
+    # Find rotation matrix from previous axis and angle
+    r = rotMatrixFromAxisAngle(w, theta)
+
+    # Rotate v twiceby 90 degrees
+    v1 = np.matmul(r, v)
+    v2 = np.matmul(r, v1)
+
+    # Plot everything
     ax = initPlot()
-    plotLine(ax, w_norm, color='black', linestyle='-.')
+    plotLine(ax, w, color='black', linestyle='--')
+    plotLine(ax, v, color='magenta', coord=True)
+    plotLine(ax, v1, color='cyan')
+    plotLine(ax, v2, color='yellow')
 
     plt.show()
--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ Created on Wed Jun 20 14:30:20 2018
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import math
 
 """ Performs multiplication from quaternions q1 and q2, returning the quaternion result q. """
 def quatProd(q1, q2):
@@ -23,8 +24,24 @@ def quatProd(q1, q2):
     q[1:] = u0*v + v0*u + np.cross(u, v)
     return q
 
+""" m is a 3-by-1 column vector.
+    Returns a antissymetric 3-by-3 matrix J(m) that substitues cross product from m by a matrix multiplication.
+    Thus, cross product m x n becomes the matrices multiplication J(m)*n """
+def J( m ):
+    out = np.zeros((3, 3))
+    out[0][1] = -m.item(2); out[1][0] =  m.item(2);
+    out[0][2] =  m.item(1); out[2][0] = -m.item(1);
+    out[1][2] = -m.item(0); out[2][1] =  m.item(0);
+    return out
+
+""" Returns a rotation matrix from a given axis and angle. """
+def rotMatrixFromAxisAngle(axis, angle):
+    # Uses the Rodriges formula, as follows...
+    R = np.eye(3) + math.sin(angle)*J(axis) + ( 1-math.cos(angle) )*np.matmul(J(axis), J(axis))
+    return R
+
 """ Plot a line between two points given an axis with a specific color"""
-def plotLine(ax, p2, p1=np.zeros(3).reshape(3, 1), color='black', label='', coord=True, linestyle='-'):
+def plotLine(ax, p2, p1=np.zeros(3).reshape(3, 1), color='black', label='', coord=False, linestyle='-'):
     ax.plot([p1[0][0], p2[0][0]],
              [p1[1][0], p2[1][0]],
              [p1[2][0], p2[2][0]],
@@ -32,8 +49,8 @@ def plotLine(ax, p2, p1=np.zeros(3).reshape(3, 1), color='black', label='', coor
              linestyle=linestyle)
     if label:
         ax.text(p2[0][0], p2[1][0], p2[2][0] , label)
-    elif coord:
-        ax.text(p2[0][0], p2[1][0], p2[2][0] , '({:.3f}, {:.3f}, {:.3f})'.format(p2[0][0], p2[1][0], p2[2][0]))
+    if coord:
+        ax.text(p2[0][0], p2[1][0], p2[2][0] - 0.1 , '({:.3f}, {:.3f}, {:.3f})'.format(p2[0][0], p2[1][0], p2[2][0]))
 
 """ Init and setup axis. Needed before plotting vectors. """
 def initPlot():
